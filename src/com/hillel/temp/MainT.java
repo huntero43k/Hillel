2656879dd6dde6120217ff1a738ab4d58992db52
IhorAbramovych <ihor.abramovych@gmail.com>
2022-01-08 12:45:47 +0200

    new commit again

---
+++ src/com/hillel/temp/MainT.java
@@ -0,0 +1,11 @@
+package com.hillel.temp;
+
+public class MainT {
+    public static void main(String[] args) {
+        String a = "Hello Mr.Abramovych";
+        int n = a.length();
+        System.out.println(n);
+    }
+
+
+}
