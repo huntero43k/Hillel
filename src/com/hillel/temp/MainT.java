00e575e2e4cf7c41e8dbfe8c8326fa17438987d0
IhorAbramovych <ihor.abramovych@gmail.com>
2022-01-09 02:34:10 +0200

    new commin new

--- src/com/hillel/temp/MainT.java
+++ src/com/hillel/temp/MainT.java
@@ -2,10 +2,17 @@ package com.hillel.temp;
 
 public class MainT {
     public static void main(String[] args) {
-        String a = "Hello Mr.Abramovych";
-        int n = a.length();
-        System.out.println(n);
+        //String a = "Hello Mr.Abramovych";
+        //int n = a.length();
+        //System.out.println(n);
+//        double a = 0;
+//        double b = 0;
+//
+//        System.out.println(a / b);
+        long a = 1234567890;
+        float b = a;
+        double c = a;
+        System.out.println(b);
+        System.out.println(c);
     }
-
-
 }
