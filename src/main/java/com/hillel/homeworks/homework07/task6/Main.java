d1c9badf7e2ebcb8a691f24cf784456c22cb20ce
Игорь Абрамович <i.abramovich@ukravto.ua>
2022-02-18 13:12:03 +0200

    HW07 Refactoring

--- src/main/java/com/hillel/homeworks/homework07/task6/Main.java
+++ src/main/java/com/hillel/homeworks/homework07/task6/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a, b, c, d, e, f;
-        int x, y;
+        double a, b, c, d, e, f;
+        double x, y;
 
         a = scanner.nextInt();
         b = scanner.nextInt();
